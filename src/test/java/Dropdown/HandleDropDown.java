66d50e72e61c1f9a90a3b1b639a6d1ac2c4b0f1d
arpit7417 <singhatul2833@gmail.com>
2026-04-17 23:08:39 +0530

    some chages in the class

--- src/test/java/Dropdown/HandleDropDown.java
+++ src/test/java/Dropdown/HandleDropDown.java
@@ -2,11 +2,16 @@ package Dropdown;
 
 import java.time.Duration;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+
+
 
 public class HandleDropDown {
 public static void main(String[] args) {
+	
+	System.out.println(" this is drop down practice ");
 	
 	System.out.println(" this is drop down");
 }
