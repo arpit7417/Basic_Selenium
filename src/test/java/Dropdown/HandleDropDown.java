f5bbfba5c883e050028d1a01cdc8095a8a6d717b
arpit7417 <singhatul2833@gmail.com>
2026-04-17 16:28:35 +0530

    new class added

---
+++ src/test/java/Dropdown/HandleDropDown.java
@@ -0,0 +1,14 @@
+package Dropdown;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleDropDown {
+public static void main(String[] args) {
+	
+	System.out.println(" this is drop down");
+}
+	
+}
